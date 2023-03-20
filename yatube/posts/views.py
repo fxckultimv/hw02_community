031078ef3ad3f386a3447618c1075daf44fb207d
Mikhail Komissarov <datradexon@gmail.com>
2023-03-20 23:43:43 +0300

    Added constants.py into root dir

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 from constants import posts_show_limit as psl
 
+
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.select_related('author')[:psl]
