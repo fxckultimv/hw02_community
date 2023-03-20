6a83310a5e5e0b40b0a0fadb6fd62cb31d97f03b
Mikhail Komissarov <datradexon@gmail.com>
2023-03-20 23:41:26 +0300

    Added constants.py into root dir

---
+++ yatube/constants.py
@@ -0,0 +1,1 @@
+posts_show_limit = 10
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-
+from constants import posts_show_limit as psl
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.select_related('author')[:10]
+    posts = Post.objects.select_related('author')[:psl]
     context = {
         'posts': posts,
     }
@@ -14,7 +14,7 @@ def index(request):
 def group_list(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[:10]
+    posts = group.posts.select_related('author')[:psl]
     context = {
         'group': group,
         'posts': posts,
