79bb3f64f626a566dd3047eb773491b20b1cf2f7
Mikhail Komissarov <datradexon@gmail.com>
2023-03-20 19:04:43 +0300

    Entirely reworked models.py + PEP8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -29,7 +29,7 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField(max_length=400, verbose_name='Текст',
-    )
+                            )
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата публикации',
